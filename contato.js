b1df47806e3b6d2fcebbc77df7db9da752f82379
eliezer maciel <eliezeromaciel@gmail.com>
2022-10-04 15:45:33 -0300

    versão 1.1 COM ERRO

--- contato.js
+++ contato.js
@@ -1,8 +1,60 @@
-async function executa (){
-    const dadosDaMensagem = recebeDadosUsuario ()
-    const dados = await enviaDadosParaApi (dadosDaMensagem)
-    mostraDadosEnviados (dados)
+function executa (){
+    analisaDados()  
 }
+
+
+function analisaDados () {
+    const divMsgUser = document.querySelector("#divMsgUser")
+    const camposMsg = [
+        {id: "#nome" , valor: document.querySelector("#nome").value},
+        {id: "#email", valor: document.querySelector("#email").value},
+        {id:"#mensagem", valor: document.querySelector("#mensagem").value} ]
+    
+    let teste = ""    
+    camposMsg.forEach( (parametro) => {
+        
+            if (parametro.valor =="") {
+            document.querySelector(parametro.id).style.border = "dashed red 2px"
+            console.log (`O campo ${parametro.id} está vazio`)        
+            
+            divMsgUser.innerHTML = `Há campos faltantes`        
+            divMsgUser.style.color = "red"
+            divMsgUser.style.border = "solid red 3px"
+            
+        } else {
+            document.querySelector(parametro.id).style.border = "solid white 1px"
+            divMsgUser.innerHTML = "Campos Ok"
+            divMsgUser.style.color = "green"
+            divMsgUser.style.border = "solid green 3px"    
+            teste += parametro
+            
+        }
+    });
+    console.log(teste[2].toString())
+    return teste        // QUERO QUE AQUI RETORNE 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function recebeDadosUsuario () {
     const todosDados = {nome:document.querySelector("#nome").value, email:document.querySelector("#email").value, mensagem:document.querySelector("#mensagem").value}
@@ -10,7 +62,8 @@ function recebeDadosUsuario () {
     if (todosDados.nome !== "" && todosDados.email !=="" && todosDados.mensagem !== "") {
         console.log("todos dados foram preenchidos. RETURN DOS DADOS")
         return todosDados
-    }  alert ("por favor digite todos dados")
+    }   
+        alert ("por favor digite todos dados")
 }
 
 async function enviaDadosParaApi (parametro) {
